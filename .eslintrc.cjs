c940e54f1edc87c1392909f18151f3bd96c872fe
Aquivion <aquivion@gmail.com>
2024-06-06 15:42:34 +0200

    update eslint configuration

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,17 +1,23 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  'extends': [
-    'plugin:react/recommended',
-    'eslint:recommended'
-  ],
-  parserOptions: {
-    ecmaVersion: 2020
-  },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-  }
+	root: true,
+	env: {
+		node: true,
+	},
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:react/recommended', // If you are using React
+		'plugin:react-hooks/recommended', // If you are using React Hooks
+	],
+	parserOptions: {
+		ecmaVersion: 2020,
+		sourceType: 'module', // Allows for the use of imports
+		ecmaFeatures: {
+			jsx: true, // Allows for parsing JSX
+		},
+	},
+	rules: {
+		'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+		'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+	},
 }
