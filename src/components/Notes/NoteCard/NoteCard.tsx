8556080cb3cd1b9abd5252468ceb32fc9f93aa2f
Aquivion <aquivion@gmail.com>
2024-06-09 19:20:36 +0200

    Add delete functionality

--- src/components/Notes/NoteCard/NoteCard.tsx
+++ src/components/Notes/NoteCard/NoteCard.tsx
@@ -3,17 +3,25 @@ import { Note } from '../../../Interfaces/Note'
 import { IonIcon } from '@ionic/react'
 
 import { trash, createOutline } from 'ionicons/icons'
+import useIonicStorageMutation from '../../../Storage/useIonicStorageMutation'
 interface NoteCardProps {
 	note: Note
 }
 function NoteCard(props: NoteCardProps) {
 	const { note } = props
+	const { remove } = useIonicStorageMutation()
+
+	const onClickDelete = () => {
+		console.log('delete clicked!')
+		remove(note.id)
+	}
 	return (
 		<div className="flex border border-white px-2 py-3 rounded-md items-center justify-between">
 			<p>{note.title}</p>
 			<div>
-				<IonIcon icon={createOutline} className="text-xl hover:text-green-400 px-2" />
-				<IonIcon icon={trash} className="text-xl hover:text-red-400" />
+				<button onClick={onClickDelete}>
+					<IonIcon icon={trash} className="text-xl hover:text-red-400" />
+				</button>
 			</div>
 		</div>
 	)
