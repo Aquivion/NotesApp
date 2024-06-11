0490ba576ee7d36aa6edfbf5f933d7075befa3fe
Aquivion <aquivion@gmail.com>
2024-06-11 10:52:38 +0200

    Use IonButton for notelist item

--- src/components/Notes/NoteCard/NoteCard.tsx
+++ src/components/Notes/NoteCard/NoteCard.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { Note } from '../../../Interfaces/Note'
-import { IonIcon } from '@ionic/react'
+import { IonButton, IonIcon } from '@ionic/react'
 
 import { trash, createOutline } from 'ionicons/icons'
 import useIonicStorageMutation from '../../../Storage/useIonicStorageMutation'
+import './NoteCard.scss'
+
 interface NoteCardProps {
 	note: Note
 }
@@ -15,15 +17,27 @@ function NoteCard(props: NoteCardProps) {
 		console.log('delete clicked!')
 		remove(note.id)
 	}
+
+	const onEditNote = () => {
+		console.log('edit')
+	}
 	return (
-		<div className="flex border border-white px-2 py-3 rounded-md items-center justify-between">
-			<p>{note.title}</p>
-			<div>
-				<button onClick={onClickDelete}>
-					<IonIcon icon={trash} className="text-xl hover:text-red-400" />
-				</button>
+		<IonButton
+			fill="outline"
+			onClick={onEditNote}
+			className="w-full normal-case justify-between text-white m-0 ion-button-border-white"
+		>
+			{/* <div className="flex border border-white px-2 py-3 rounded-md items-center justify-between"> */}
+			<div className="flex flex-row items-center justify-between w-full">
+				<p>{note.title}</p>
+				<div>
+					<button onClick={onClickDelete}>
+						<IonIcon slot="start" icon={trash} className="text-xl hover:text-red-400" />
+					</button>
+				</div>
 			</div>
-		</div>
+			{/* </div> */}
+		</IonButton>
 	)
 }
 
